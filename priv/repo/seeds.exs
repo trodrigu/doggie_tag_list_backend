5fd19642fde256c257d0b0460f0441fd46271541
trodrigu <trodriguez91@icloud.com>
2018-11-28 11:25:54 -0800

    update seeds with insert user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias DoggieTagCx.User
+alias DoggieTagCx.Repo
+
+if Mix.env() == :dev do
+  changes = %{name: "some user", email: "test@test.com", password: "supersecret"}
+
+  %User{}
+  |> User.registration_changeset(changes)
+  |> Repo.insert!()
+end
